0a2d63f069f8cc762f769173a5b58667a381ffb5
Andrzej J. <gh@jarmoniuk.nl>
2025-10-18 08:48:31 +0200

    Simplification re artifacts

--- versions-common/src/main/java/org/codehaus/mojo/versions/utils/ArtifactFactory.java
+++ versions-common/src/main/java/org/codehaus/mojo/versions/utils/ArtifactFactory.java
@@ -4,10 +4,6 @@ import javax.inject.Inject;
 import javax.inject.Named;
 
 import java.io.File;
-import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Supplier;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.artifact.Artifact;
@@ -23,64 +19,6 @@ import org.apache.maven.model.Dependency;
 @Named
 public class ArtifactFactory {
     private final ArtifactHandlerManager artifactHandlerManager;
-    private final Map<ArtifactIdentifier, Artifact> artifactCache = new ConcurrentHashMap<>();
-
-    private static class ArtifactIdentifier {
-        String groupId;
-        String artifactId;
-        String version;
-        String type;
-        String classifier;
-        String scope;
-        boolean optional;
-
-        ArtifactIdentifier(
-                String groupId,
-                String artifactId,
-                String version,
-                String type,
-                String classifier,
-                String scope,
-                boolean optional) {
-            this.groupId = groupId;
-            this.artifactId = artifactId;
-            this.version = version;
-            this.type = type;
-            this.classifier = classifier;
-            this.scope = scope;
-            this.optional = optional;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (!(o instanceof ArtifactIdentifier)) {
-                return false;
-            }
-            ArtifactIdentifier that = (ArtifactIdentifier) o;
-            return optional == that.optional
-                    && Objects.equals(groupId, that.groupId)
-                    && Objects.equals(artifactId, that.artifactId)
-                    && Objects.equals(version, that.version)
-                    && Objects.equals(type, that.type)
-                    && Objects.equals(classifier, that.classifier)
-                    && Objects.equals(scope, that.scope);
-        }
-
-        @Override
-        public int hashCode() {
-            int result = groupId.hashCode();
-            result = 31 * result + (artifactId != null ? artifactId.hashCode() : 47);
-            result = 31 * result + (version != null ? version.hashCode() : 89);
-            result = 31 * result + (type != null ? type.hashCode() : 127);
-            result = 31 * result + (classifier != null ? classifier.hashCode() : 167);
-            result = 31 * result + (scope != null ? scope.hashCode() : 223);
-            result = 31 * result + (optional ? 233 : 0);
-            return result;
-        }
-    }
 
     /**
      * Constructs a new instance
@@ -89,10 +27,6 @@ public class ArtifactFactory {
     @Inject
     public ArtifactFactory(ArtifactHandlerManager artifactHandlerManager) {
         this.artifactHandlerManager = artifactHandlerManager;
-    }
-
-    private Artifact createArtifact(ArtifactIdentifier id, Supplier<Artifact> supplier) {
-        return artifactCache.computeIfAbsent(id, __ -> supplier.get());
     }
 
     /**
@@ -117,22 +51,19 @@ public class ArtifactFactory {
             String classifier,
             String scope,
             boolean optional) {
-        return createArtifact(
-                new ArtifactIdentifier(groupId, artifactId, version, type, classifier, scope, optional), () -> {
-                    try {
-                        return new DefaultArtifact(
-                                groupId,
-                                artifactId,
-                                VersionRange.createFromVersionSpec(StringUtils.isNotBlank(version) ? version : "[0,]"),
-                                scope,
-                                type,
-                                classifier,
-                                artifactHandlerManager.getArtifactHandler(type),
-                                optional);
-                    } catch (InvalidVersionSpecificationException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+        try {
+            return new DefaultArtifact(
+                    groupId,
+                    artifactId,
+                    VersionRange.createFromVersionSpec(StringUtils.isNotBlank(version) ? version : "[0,]"),
+                    scope,
+                    type,
+                    classifier,
+                    artifactHandlerManager.getArtifactHandler(type),
+                    optional);
+        } catch (InvalidVersionSpecificationException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -152,36 +83,17 @@ public class ArtifactFactory {
      * @return the created {@link Artifact} instance
      */
     public Artifact createArtifact(Dependency dependency) {
-        return createArtifact(
-                new ArtifactIdentifier(
-                        dependency.getGroupId(),
-                        dependency.getArtifactId(),
-                        dependency.getVersion(),
-                        dependency.getType(),
-                        dependency.getClassifier(),
-                        dependency.getScope(),
-                        false),
-                () -> {
-                    try {
-                        Artifact artifact = new DefaultArtifact(
-                                dependency.getGroupId(),
-                                dependency.getArtifactId(),
-                                VersionRange.createFromVersionSpec(
-                                        StringUtils.isNotBlank(dependency.getVersion())
-                                                ? dependency.getVersion()
-                                                : "[0,]"),
-                                dependency.getScope(),
-                                dependency.getType(),
-                                dependency.getClassifier(),
-                                artifactHandlerManager.getArtifactHandler(dependency.getType()),
-                                dependency.isOptional());
-                        if (Artifact.SCOPE_SYSTEM.equals(dependency.getScope()) && dependency.getSystemPath() != null) {
-                            artifact.setFile(new File(dependency.getSystemPath()));
-                        }
-                        return artifact;
-                    } catch (InvalidVersionSpecificationException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
+        Artifact artifact = createArtifact(
+                dependency.getGroupId(),
+                dependency.getArtifactId(),
+                dependency.getVersion(),
+                dependency.getType(),
+                dependency.getClassifier(),
+                dependency.getScope(),
+                dependency.isOptional());
+        if (Artifact.SCOPE_SYSTEM.equals(dependency.getScope()) && dependency.getSystemPath() != null) {
+            artifact.setFile(new File(dependency.getSystemPath()));
+        }
+        return artifact;
     }
 }
